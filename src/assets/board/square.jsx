e808f07be20eb15164aa2cbce4c051e02fabe091
Rafi Sutrisno <mrafis2003@gmail.com>
2024-05-21 15:55:23 +0700

    feat: square recv value from board

--- src/assets/board/square.jsx
+++ src/assets/board/square.jsx
@@ -1,13 +1,16 @@
 import "./style.css";
+import { useState } from "react";
 
-export default function Square({ value }) {
-  function handleClick() {
-    console.log("clicked!");
-  }
+export default function Square({ value, onSquareClick }) {
+  //   const [value, setValue] = useState(null);
+
+  //   function handleClick() {
+  //     setValue("X");
+  //   }
 
   return (
     <>
-      <button className="square" onClick={handleClick}>
+      <button className="square" onClick={onSquareClick}>
         {value}
       </button>
     </>
