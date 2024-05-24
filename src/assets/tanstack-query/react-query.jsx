92bac76f2c3d3e82073ecba43f0c0137a9f2241b
Rafi Sutrisno <mrafis2003@gmail.com>
2024-05-24 20:01:42 +0700

    feat: add react-query

--- src/assets/tanstack-query/react-query.jsx
+++ src/assets/tanstack-query/react-query.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 const reactQuery = () => {
+  const {
+    data: comments,
+    isLoading,
+    error,
+  } = useQuery({
+    queryFn: () =>
+      fetch("https://jsonplaceholder.typicode.com/comments?_limit=10").then(
+        (res) => res.json()
+      ),
+    queryKey: ["comments"],
+  });
+
+  // Show a loading message while data is fetching
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  // to handle error
+  if (error) {
+    return <div className="error">Error: error fetching</div>;
+  }
   return (
     <>
-      <div></div>
+      <h1 className="title">Email address of users</h1>
+      {comments.map((comment) => (
+        <h2 key={comment.id} className="users">
+          {comment.id}.{comment.email}
+        </h2>
+      ))}
     </>
   );
 };
